845690ea142425b612eb6ce5637d521bc03ace17
GamerBoss101 <adithcool.5@gmail.com>
2025-09-27 02:00:06 -0400

    MapBox test 1

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,13 +1,26 @@
-import Image from "next/image";
+"use client";
 
 import Map from 'react-map-gl/mapbox';
 
 import 'mapbox-gl/dist/mapbox-gl.css';
 
 export default function Home() {
+
+	let width = window.innerWidth;
+	let height = window.innerHeight;
+
   return (
 	<div>
-		:)
+		<Map
+			mapboxAccessToken="pk.eyJ1IjoicGllbG9yZDc1NyIsImEiOiJjbWcxdTd6c3AwMXU1MmtxMDh6b2l5amVrIn0.5Es0azrah23GX1e9tmbjGw"
+			initialViewState={{
+				longitude: -122.4,
+				latitude: 37.8,
+				zoom: 14
+			}}
+			style={{width, height}}
+			mapStyle="mapbox://styles/mapbox/streets-v9"
+		/>
 	</div>
   );
 }
